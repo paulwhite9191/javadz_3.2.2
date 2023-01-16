63ef5ad8dd863a0d624dbbe3f9b4feac8bb1b9cc
Paul White <paratrak@list.ru>
2023-01-17 00:00:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int clientAccount = 330;
+        int amount = 1551;
+
+        if (amount <= 1000) {
+            System.out.println(" Сумма счёта: " + (clientAccount + amount));
+        } else {
+            int bonus = amount / 100;
+            System.out.println(" Сумма счёта: " + (clientAccount + amount) + ", Сумма бонусов: " + bonus + ", Итого: "
+                    + (clientAccount + amount + bonus));
+
+
+        }
+
+
+    }
+}
