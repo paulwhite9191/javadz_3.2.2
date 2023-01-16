0775ac194a7740498c797625e3158d9101a58fa6
paulwhite9191 <120777547+paulwhite9191@users.noreply.github.com>
2023-01-17 00:04:30 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,5 @@ public class Main {
             System.out.println(" Сумма счёта: " + (clientAccount + amount) + ", Сумма бонусов: " + bonus + ", Итого: "
                     + (clientAccount + amount + bonus));
         }
-
-
     }
 }
